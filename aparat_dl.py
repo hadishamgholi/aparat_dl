9ebc1a0f4cde3a52098262de8003904bd5070eed
shiravand <sm96dd@gmail.com>
2016-12-15 04:50:22 +0330

    code improvement

--- aparat_dl.py
+++ aparat_dl.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import scraper
 import downloader
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    argv = sys.argv
-    print(argv[1])
     links = scraper.get_links(argv[1])
     print(links)
