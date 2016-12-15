25f20377d5550bab8368492b063ff1d24d3eb185
shiravand <sm96dd@gmail.com>
2016-12-15 05:05:45 +0330

    add aria2 downloader functionality

--- aparat_dl.py
+++ aparat_dl.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 import scraper
 import downloader
-from sys import argv
+from sys import argv, exit
 
 
 if __name__ == "__main__":
     links = scraper.get_links(argv[1])
-    print(links)
+    i = 1
+    for link in links:
+        print(i, link[0])
+        print(link[1])
+        i = i + 1
+    inp = int(input("Select Quality: "))
+    if inp < 1 or inp > len(links):
+        print("Invalid selection!")
+        sys.exit(1)
+    link = links[inp-1][1]
+    downloader.download_one(link)
--- downloader.py
+++ downloader.py
@@ -5,7 +5,9 @@ def download_one(link):
     cmd = 'youtube-dl --external-downloader aria2c \
             --external-downloader-args \
             --max-connection-per-server=16'
+    print('$$> Download started:')
     os.system(cmd + " " + link)
+    print('$$> Download finished :)')
 
 def download_all(links):
     for link in links:
