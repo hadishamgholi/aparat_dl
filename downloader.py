eeaaeed5e54ade4298c581f9b865d1e4bdff8891
shiravand <sm96dd@gmail.com>
2016-12-15 13:09:40 +0330

    remove youtube_dl

--- downloader.py
+++ downloader.py
@@ -2,9 +2,7 @@ from sys import argv
 import os
 
 def download_one(link):
-    cmd = 'youtube-dl --external-downloader aria2c \
-            --external-downloader-args \
-            --max-connection-per-server=16'
+    cmd = 'aria2c --max-connection-per-server=16'
     print('$$> Download started:')
     os.system(cmd + " " + link)
     print('$$> Download finished :)')
